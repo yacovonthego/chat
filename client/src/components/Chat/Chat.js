ced3c8c7def377bc84a5aea9bb72d528255740fa
yakov <yakovonthego@gmail.com>
2019-10-25 10:47:53 +0300

    Message box styles.

--- client/src/components/Chat/Chat.js
+++ client/src/components/Chat/Chat.js
@@ -1,6 +1,8 @@
 import React, { useState, useEffect } from 'react';
 import io from "socket.io-client";
 
+import './Chat.css'
+
 const ENDPOINT 	= 'localhost:5000';
 const socket 	= io(ENDPOINT);
 
@@ -41,11 +43,25 @@ const Chat = (props) => {
 
 	return (
 		<div>
-			<h1>CHAT</h1>
-			{users}
-			<div className="container-chat">
-				<input type="text" className="input input-chat"/>
-				<button className="button button-chat">Send</button>
+			<div className="outer p-medium">
+				<h1 className="roboto title-chat">CHAT: {room}</h1>
+				<div className="container-chat">
+					<div className="container-people">
+
+					</div>
+					<div className="container-box">
+						<div className="message-box">
+
+						</div>
+						<div className="container-input">
+							<textarea 
+								placeholder="Enter your message..."
+								type="text" className="input input-chat roboto"
+							/>
+							<button className="button button-chat p-small mt20 roboto">Send</button>
+						</div>
+					</div>
+				</div>
 			</div>
 		</div>
 	)
